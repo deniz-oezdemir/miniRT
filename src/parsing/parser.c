1e372ca93e3157438fffb0557766311a092022a8
deniz <oezdemir.de@gmail.com>
2024-05-03 17:26:42 +0200

    pseudocode parse_objects

--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -6,7 +6,7 @@
 /*   By: denizozd <denizozd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 17:08:26 by denizozd          #+#    #+#             */
-/*   Updated: 2024/05/03 17:10:46 by denizozd         ###   ########.fr       */
+/*   Updated: 2024/05/03 17:26:10 by denizozd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void	parse_objects(t_minirt *data)
 {
-	parse_ambient_light(data);
+	//loop
+		//gnl
+		//split
+		// if A
+			//parse_ambient_light(data); //atoi
+		//else if C
+			//parse_cylindr
+		//else if ...
+			///...
+		//else
+			//throw error: incorrect scene
+		//free gnl, free split
 }
