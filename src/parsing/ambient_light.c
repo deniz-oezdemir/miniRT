0354912ad9f6301ce804d8d6f22cebf62e33a21d
tiziaco <tiziano.iaco@gmail.com>
2024-05-09 11:02:34 +0200

    define ambient light validation

--- src/parsing/ambient_light.c
+++ src/parsing/ambient_light.c
@@ -3,27 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ambient_light.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: denizozd <denizozd@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 17:10:53 by denizozd          #+#    #+#             */
-/*   Updated: 2024/05/07 12:15:05 by denizozd         ###   ########.fr       */
+/*   Updated: 2024/05/08 17:01:32 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../../include/minirt.h"
 
-void	parse_ambient_light(t_minirt *data, t_list *al)
+void	parse_ambient_light(t_minirt *data, t_list **al)
 {
 	t_amblight *amblight;
 
 	amblight = ft_calloc(1, sizeof(t_amblight));
 	if (!amblight)
 		printf("Error: allocation failed\n");
-	amblight->name = al->content;
-	amblight->intensity = atof(al->next->content);
-	amblight->r = ft_atoi(al->next->next->content);
-	amblight->g = ft_atoi(al->next->next->next->content);
-	amblight->b = ft_atoi(al->next->next->next->next->content);
+	amblight->name = (*al)->content;
+	amblight->intensity = check_intensity((*al)->next->content);
+	amblight->r = check_rgb((*al)->next->next->content);
+	amblight->g = check_rgb((*al)->next->next->next->content);
+	amblight->b = check_rgb((*al)->next->next->next->next->content);
 	ft_lstadd_back(&(data->objects), ft_lstnew(amblight));
+	(*al) = (*al)->next->next->next->next;
+	printf("Ambient light OK\n");
 	//printf("amblight: name: %s, intensity: %f, r: %d\n", amblight->name, amblight->intensity, amblight->r);
 }
