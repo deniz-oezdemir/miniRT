b6ad5987fae8c1bc2f829e3b58283c2063b623eb
tiziaco <tiziano.iaco@gmail.com>
2024-05-09 11:03:33 +0200

    update main

--- src/main.c
+++ src/main.c
@@ -6,37 +6,36 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 15:05:31 by denizozd          #+#    #+#             */
-/*   Updated: 2024/05/08 00:33:44 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/05/08 18:08:36 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../include/minirt.h"
 
+void print_node(void *data) {
+    t_amblight *amblight = (t_amblight *)data;
+    printf("Name: %s\n", amblight->name);
+    printf("Intensity: %f\n", amblight->intensity);
+    printf("RGB: (%d, %d, %d)\n", amblight->r, amblight->g, amblight->b);
+	printf("\n");
+}
+
 int	main(int argc, char **argv)
 {
 	t_minirt	*data;
 
-	/* if (argc != 2)
+	if (argc != 2)
 	{
-		//ft_printf("ERROR: please check input arguments!");
+		printf("ERROR: please check input arguments!");
 		return (EXIT_FAILURE);
-	} */
+	}
 	data = init_mlx();
 
-	data->fd = open(argv[1], O_RDONLY, 0);
-	if (data->fd < 0)
-		printf("Error: opening file\n"); //add: close fd, free data
+	parse(data, argv[1]);
+	printf("\nScene objects:\n"); //check
+	ft_lstiter(data->objects, print_node); //check
 
-	file_to_scene_list(data);
-	close(data->fd); //change if file needed elsewhere
-	printf("scene list's contents:\n"); //check
-	ft_lstiter(data->scene, printf); //check
-
-	file_to_scene_list(data);
-	t_amblight *al = (t_amblight *)((t_list *)(data->objects)->content); //check
-	printf("\namblight:\nname: %s - intensity: %f - r: %d - g: %d - b: %d\n", al->name, al->intensity, al->r, al->g, al->b); //check
-
-	init_window(data);
-	mlx_loop(data->mlx_ptr);
+	/* init_window(data);
+	mlx_loop(data->mlx_ptr); */
 	return (0);
 }
