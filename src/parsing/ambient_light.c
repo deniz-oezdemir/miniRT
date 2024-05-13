a7e08107a0e6689ee328731e404c41c0781a10bb
tiziaco <tiziano.iaco@gmail.com>
2024-05-13 15:07:54 +0200

    finalize amblight validation

--- src/parsing/ambient_light.c
+++ src/parsing/ambient_light.c
@@ -6,11 +6,19 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 17:10:53 by denizozd          #+#    #+#             */
-/*   Updated: 2024/05/13 13:24:55 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/05/13 14:50:11 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../../include/minirt.h"
+
+static bool	validate_amblight(t_amblight *amblight)
+{
+	if ((amblight->intensity < 0)
+		|| (amblight->r < 0) || (amblight->g < 0) || (amblight->b < 0))
+		return false;
+	return true;
+}
 
 void	parse_ambient_light(t_minirt *data, t_list **al)
 {
@@ -24,6 +32,8 @@ void	parse_ambient_light(t_minirt *data, t_list **al)
 	amblight->r = check_rgb((*al)->next->next->content);
 	amblight->g = check_rgb((*al)->next->next->next->content);
 	amblight->b = check_rgb((*al)->next->next->next->next->content);
+	if (!validate_amblight(amblight))
+		return ; // TODO: print an error and clean everything
 	ft_lstadd_back(&(data->objects), ft_lstnew(amblight));
 	(*al) = (*al)->next->next->next->next;
 	printf("Ambient light OK\n");
