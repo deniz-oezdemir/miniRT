2f8b859c5f5f0e103cef64c28946823bc42b18f5
tiziaco <tiziano.iaco@gmail.com>
2024-05-13 19:07:12 +0200

    refactor code

--- src/parsing/ambient_light.c
+++ src/parsing/ambient_light.c
@@ -6,7 +6,7 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 17:10:53 by denizozd          #+#    #+#             */
-/*   Updated: 2024/05/13 14:50:11 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/05/13 18:53:16 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,21 @@ static bool	validate_amblight(t_amblight *amblight)
 	return true;
 }
 
-void	parse_ambient_light(t_minirt *data, t_list **al)
+void	parse_ambient_light(t_minirt *data, t_list **input_lst)
 {
-	t_amblight *amblight;
+	t_amblight	*amblight;
 
 	amblight = ft_calloc(1, sizeof(t_amblight));
 	if (!amblight)
 		printf("Error: allocation failed\n");
-	amblight->name = (*al)->content;
-	amblight->intensity = check_intensity((*al)->next->content);
-	amblight->r = check_rgb((*al)->next->next->content);
-	amblight->g = check_rgb((*al)->next->next->next->content);
-	amblight->b = check_rgb((*al)->next->next->next->next->content);
+	amblight->name = (*input_lst)->content;
+	amblight->intensity = check_intensity(get_nth_content(*input_lst, 1));
+	amblight->r = check_rgb(get_nth_content(*input_lst, 2));
+	amblight->g = check_rgb(get_nth_content(*input_lst, 3));
+	amblight->b = check_rgb(get_nth_content(*input_lst, 4));
 	if (!validate_amblight(amblight))
 		return ; // TODO: print an error and clean everything
 	ft_lstadd_back(&(data->objects), ft_lstnew(amblight));
-	(*al) = (*al)->next->next->next->next;
+	move_to_nth_node(input_lst, 4);
 	printf("Ambient light OK\n");
 }
--- src/parsing/camera.c
+++ src/parsing/camera.c
@@ -6,7 +6,7 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 17:44:34 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/05/13 15:05:53 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/05/13 18:53:19 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,24 @@ static bool	validate_camera(t_camera *camera)
 	return (true);
 }
 
-void	parse_camera(t_minirt *data, t_list **scene)
+void	parse_camera(t_minirt *data, t_list **input_lst)
 {
-	t_camera *camera;
+	t_camera	*camera;
 
 	camera = ft_calloc(1, sizeof(t_camera));
 	if (!camera)
 		printf("Error: allocation failed\n");
-	camera->name = (*scene)->content;
-	camera->x = check_coordinate((*scene)->next->content);
-	camera->y = check_coordinate((*scene)->next->next->content);
-	camera->z = check_coordinate((*scene)->next->next->next->content);
-	camera->xv = check_vec3((*scene)->next->next->next->next->content);
-	camera->yv = check_vec3((*scene)->next->next->next->next->next->content);
-	camera->zv = check_vec3((*scene)->next->next->next->next->next->next->content);
-	camera->fov = check_fov((*scene)->next->next->next->next->next->next->next->content);
+	camera->name = (*input_lst)->content;
+	camera->x = check_coordinate(get_nth_content(*input_lst, 1));
+	camera->y = check_coordinate(get_nth_content(*input_lst, 2));
+	camera->z = check_coordinate(get_nth_content(*input_lst, 3));
+	camera->xv = check_vec3(get_nth_content(*input_lst, 4));
+	camera->yv = check_vec3(get_nth_content(*input_lst, 5));
+	camera->zv = check_vec3(get_nth_content(*input_lst, 6));
+	camera->fov = check_fov(get_nth_content(*input_lst, 7));
 	if (!validate_camera(camera))
 		return ; // TODO: print an error and clean everything
 	ft_lstadd_back(&(data->objects), ft_lstnew(camera));
-	(*scene) = (*scene)->next->next->next->next->next->next->next;
+	move_to_nth_node(input_lst, 7);
 	printf("Camera light OK\n");
 }
