9d948d151305abae926d813a6a13275050b1d8fe
tiziaco <tiziano.iaco@gmail.com>
2024-05-14 16:06:35 +0200

    add error codes header

---
+++ include/errors.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 15:49:09 by tiacovel          #+#    #+#             */
+/*   Updated: 2024/05/14 15:55:06 by tiacovel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# define AMB_LIGHT_ERR  10
+# define CAMERA_ERR     11
+# define CYLINDER_ERR   12
+# define LIGHT_ERR      13
+# define PLANE_ERR      14
+# define SPHERE_ERR     15
