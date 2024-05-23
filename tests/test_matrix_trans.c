49997514c41bcf5dfc18e6755539874a5be87069
deniz <oezdemir.de@gmail.com>
2024-05-23 16:10:58 +0200

    iq

--- tests/test_matrix_trans.c
+++ tests/test_matrix_trans.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_matrix_trans.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: denizozd <denizozd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 16:30:58 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/05/21 16:33:21 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/05/23 16:01:34 by denizozd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void	test_mtx_trans(void)
 	printf("\nMultiplying point (-3, 4, 5) by translation (5, -3, 2):\n");
 	t_vec3 p = {-3, 4, 5, 1};
 	print_vec3(mult_pnt_mtx(p, translation_mtx(5, -3, 2)), "(2, 1, 7, 1) expected");
-	printf("\nTBD Multiplying by the inverse of a translation matrix (p. 45): @Tiziano inverse needed\n");
+	printf("\nTBD Multiplying by the inverse of a translation matrix: \n");
+	print_vec3(mult_pnt_mtx(p, invert_mtx(translation_mtx(5, -3, 2))), "(-8, 7, 3, 1) expected");
 	printf("\nMultiplying vector (-3, 4, 5) by translation (5, -3, 2):\n");
 	t_vec3 v = {-3, 4, 5, 0};
 	print_vec3(mult_pnt_mtx(v, translation_mtx(5, -3, 2)), "(-3, 4, 5, 0) expected");
@@ -30,7 +31,8 @@ void	test_mtx_trans(void)
 	p = (t_vec3){0, 1, 0, 1};
 	print_vec3(mult_pnt_mtx(p, rot_x(PI/4)), "Half a quarter: (0, √2/2, √2/2, 1) expected");
 	print_vec3(mult_pnt_mtx(p, rot_x(PI/2)), "Full quarter: (0, 0, 1, 1) expected");
-	printf("\nTBD inverse of rotation (p. 49): @Tiziano inverse needed\n");
+	printf("\nInverse of rotation:\n");
+	print_vec3(mult_pnt_mtx(p, invert_mtx(rot_x(PI/4))), "Half a quarter: (0, √2/2, -√2/2, 1) expected");
 
 	printf("\n\nRotating point (0, 0, 1) around the y axis:\n");
 	p = (t_vec3){0, 0, 1, 1};
@@ -42,4 +44,23 @@ void	test_mtx_trans(void)
 	print_vec3(mult_pnt_mtx(p, rot_z(PI/4)), "Half a quarter: (-√2/2, √2/2, 0, 1) expected");
 	print_vec3(mult_pnt_mtx(p, rot_z(PI/2)), "Full quarter: (-1, 0, 0, 1) expected");
 
+	printf("\n\nRotating point (0, 1, 0) around the z axis:\n");
+	p = (t_vec3){0, 1, 0, 1};
+	print_vec3(mult_pnt_mtx(p, rot_z(PI/4)), "Half a quarter: (-√2/2, √2/2, 0, 1) expected");
+	print_vec3(mult_pnt_mtx(p, rot_z(PI/2)), "Full quarter: (-1, 0, 0, 1) expected");
+
+printf("\n\nA scaling matrix (2, 3, 4) applied to a point (-4, 6, 8):\n");
+	p = (t_vec3){-4, 6, 8, 1};
+	print_vec3(mult_pnt_mtx(p, scaling(2, 3, 4)), "(-8, 18, 32) expected");
+
+printf("\n\nA scaling matrix (2, 3, 4) applied to a vector (-4, 6, 8):\n");
+	p = (t_vec3){-4, 6, 8, 0};
+	print_vec3(mult_pnt_mtx(p, scaling(2, 3, 4)), "(-8, 18, 32) expected");
+
+printf("\nMultiplying by the inverse of a scaling matrix\n");
+	print_vec3(mult_pnt_mtx(p, invert_mtx(scaling(2, 3, 4))), "(-2, 2, 2) expected");
+
+printf("\n\nReflection is scaling by a negativ value (-1, 1, 1) applied to a point (2, 3, 4):\n");
+	p = (t_vec3){2, 3, 4, 1};
+	print_vec3(mult_pnt_mtx(p, scaling(-1, 1, 1)), "(-2, 3, 4) expected");
 }
