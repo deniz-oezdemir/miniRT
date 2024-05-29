882fa905524746eb243af6737e542e8d09a05acc
tiziaco <tiziano.iaco@gmail.com>
2024-05-29 15:29:07 +0200

    add test intersect 2 spheres

---
+++ tests/test_intersect_2.c
@@ -0,0 +1,21 @@
+# include "../include/minirt.h"
+# include "../include/tests.h"
+
+void test_inter_2(t_minirt *data)
+{
+	printf ("\n*** Test intersections 2 spheres ***\n\n");
+	free_world(data->world);
+	parse(data, "scene_2_sphere.rt");
+
+	t_ray ray = get_ray((t_vec3){0, 0, 0, 1}, (t_vec3){0, 1, 0, 0});
+
+	printf("\nA ray intersects 2 spheres at 4 points\n");
+	intersections(data, ray);
+	printf("expected count = 4: %d\n", ft_lstsize(data->xs));
+	printf("expected inter1 = 1: %.3f\n", ((t_inter *)data->xs->content)->inter);
+	printf("expected inter2 = 3: %.3f\n", ((t_inter *)data->xs->next->content)->inter);
+	printf("expected inter3 = 3: %.3f\n", ((t_inter *)data->xs->next->next->content)->inter);
+	printf("expected inter4 = 5: %.3f\n", ((t_inter *)data->xs->next->next->next->content)->inter);
+	ft_lstclear(&data->xs, free_inter);
+
+}
