b3c4761bff3bd6ea30577fb0704ce24e69a7a870
tiziaco <tiziano.iaco@gmail.com>
2024-05-30 16:15:49 +0200

    fix bugs

--- src/render.c
+++ src/render.c
@@ -1,5 +1,22 @@
 # include "../include/minirt.h"
 
+/* Clear the intersection list "xs" and free each node. */
+void free_inter(void *content)
+{
+	t_inter *inter_content = (t_inter *)content;
+	// Set the shape pointer to NULL but don't free the shape
+	inter_content->shape = NULL;
+}
+
+static int	rgb(t_color color)
+{
+	return (
+		color.r << 16
+		| color.g << 8
+		| color.b
+	);
+}
+
 void    render_scene(t_minirt *data)
 {
 	int x;
@@ -9,37 +26,38 @@ void    render_scene(t_minirt *data)
 	double pixel_size;
 	t_vec3 position;
 	t_ray ray;
+	t_inter	hit_int;
 
 	print_instruction(data);
 	color_background(data, BACKGROUND_COLOR);
 
-	pixel_size = 0.6; //7 to be modified accordingly
+	pixel_size = 1;//7.0 / IMG_HEIGHT;
 	printf("pixel size: %f\n", pixel_size);
 	y = -1;
 	printf("Start rendering...\n");
 	while (++y < IMG_HEIGHT)
 	{
-		ft_printf("\rRendering: %d%%\n", (int)(y * 300.0 / IMG_HEIGHT));
+		printf("\rRendering: %d%%", (int)(y * 100.0 / IMG_HEIGHT));
 		world_y = IMG_HEIGHT / 2 - pixel_size * y;
 		x = -1;
 		while (++x < IMG_WIDTH)
 		{
 
 			world_x = -IMG_HEIGHT / 2 + pixel_size * x;
-			position = (t_vec3){world_x, world_y, 5, 1}; //check 10
+			position = (t_vec3){world_x, world_y, 100, 1}; //check 10
 			ray = get_ray(data->world->camera->center, vec_norm(vec_sub(position, data->world->camera->center)));
-			printf("x = %d | y = %d | w_x = %f | w_y = %f\n", x, y, world_x, world_y);
-			//print_vec3(vec_norm(vec_sub(position, data->world->camera->center)), "norm vec");
+			// printf("x = %d | y = %d | w_x = %f | w_y = %f\n", x, y, world_x, world_y);
 			intersections(data, ray);
-			if (hit(data->xs).shape != NULL)
+			hit_int = hit(data->xs);
+			if (hit_int.shape != NULL)
 			{
-				printf("hit\n");
-				color_pixel(data, x, y, 0xffffff);
+				// printf("hit\n");
+				color_pixel(data, x, y, rgb(hit_int.shape->sphere.color));
 			}
-			else
-				color_pixel(data, x, y, BACKGROUND_COLOR);
+			ft_lstclear(&data->xs, free_inter);
 		}
 	}
+	printf("\rRendering: 100%%\n");
 	mlx_put_image_to_window(data->mlx_ptr, data->mlx_win,
  		data->mlx_img->img_ptr, 200, 0);
 }
