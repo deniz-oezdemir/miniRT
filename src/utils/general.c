1ec265f36db419aadef4c5b645c1f78603c4fa98
tiziaco <tiziano.iaco@gmail.com>
2024-06-03 18:16:15 +0200

    add deg to rad function

--- src/utils/general.c
+++ src/utils/general.c
@@ -6,7 +6,7 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 12:58:44 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/05/13 13:05:43 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/06/03 17:28:21 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,9 @@ void	remove_newline(char **str)
 	newline_pos = ft_strchr(*str, '\n');
 	if (newline_pos != NULL)
 		*newline_pos = '\0';
+}
+
+double	deg_to_rad(double degrees)
+{
+	return (degrees * (PI / 180.0));
 }
