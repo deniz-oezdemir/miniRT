57095d756f424f37302545e8b205b8153e908b73
deniz <oezdemir.de@gmail.com>
2024-06-14 19:20:17 +0200

    shadows fixed

--- src/render.c
+++ src/render.c
@@ -35,7 +35,8 @@ int		is_shadow(t_minirt *data, t_vec3 light_position, t_vec3 over_point)
 	intersections(data, ray);
 	//hit_inter = hit(data->xs);
 	//ft_lstclear(&data->xs, free_inter);
-	if (data->min.inter < magnitude(v)) //ommitted is_hit flag as we initialize hit_inter.inter to MAX INT and only update it if we hit something
+	//printf("min.inter: %f\n", data->min.inter);
+	if (data->min.inter < magnitude(v) && data->min.inter > EPSILON) // check why this EPSILON fix works (was just intuition) //ommitted is_hit flag as we initialize hit_inter.inter to MAX INT and only update it if we hit something
 		return (1);
 	return (0);
 }
