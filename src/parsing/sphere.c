4e8c7face20bb9d870116d73bf6a5f473a8235b0
tiziaco <tiziano.iaco@gmail.com>
2024-06-14 10:54:16 +0200

    uncomment stuff

--- src/parsing/sphere.c
+++ src/parsing/sphere.c
@@ -6,7 +6,7 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 17:47:35 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/06/14 10:50:56 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/06/14 10:53:24 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,6 @@ void parse_sphere(t_minirt *data, t_list **input_lst)
 	if (!validate_sphere(&(sh->sphere)))
 		return (pars_error(data, SPHERE_ERR));
 	set_sphere_transform(sh);
-	print_mtx(sh->transpose);
 	ft_lstadd_back(&(data->world->objects), gc_lstnew(data, sh));
 	move_to_nth_node(input_lst, 7);
 	printf("Sphere OK\n");
