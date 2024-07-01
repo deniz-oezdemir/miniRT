d0a20493fe57ad170a2d24edbad2ef3f996ce073
deniz <oezdemir.de@gmail.com>
2024-07-01 16:38:01 +0200

    solved: perror replaced with ft_putstr_fd

--- src/handlers/error_handler.c
+++ src/handlers/error_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_handler.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: denizozd <denizozd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 09:11:20 by denizozd          #+#    #+#             */
-/*   Updated: 2024/06/27 17:41:47 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/07/01 16:36:47 by denizozd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 void	parser_error(int err_code)
 {
 	if (err_code == AMB_LIGHT_ERR)
-		perror("Error parsing Ambient light. Please check input parameters!\n");
+		ft_putstr_fd("Error parsing Ambient light. Please check input parameters!\n", 2);
 	else if (err_code == CAMERA_ERR)
-		perror("Error parsing Camera Please check input parameters!\n");
+		ft_putstr_fd("Error parsing Camera Please check input parameters!\n", 2);
 	else if (err_code == CYLINDER_ERR)
-		perror("Error parsing Cylinder. Please check input parameters!\n");
+		ft_putstr_fd("Error parsing Cylinder. Please check input parameters!\n", 2);
 	else if (err_code == LIGHT_ERR)
-		perror("Error parsing Light. Please check input parameters!\n");
+		ft_putstr_fd("Error parsing Light. Please check input parameters!\n", 2);
 	else if (err_code == PLANE_ERR)
-		perror("Error parsing Plane. Please check input parameters!\n");
+		ft_putstr_fd("Error parsing Plane. Please check input parameters!\n", 2);
 	else if (err_code == SPHERE_ERR)
-		perror("Error parsing Sphere. Please check input parameters!\n");
+		ft_putstr_fd("Error parsing Sphere. Please check input parameters!\n", 2);
 	else if (err_code == CONE_ERR)
-		perror("Error parsing Cone. Please check input parameters!\n");
+		ft_putstr_fd("Error parsing Cone. Please check input parameters!\n", 2);
 	else if (err_code == MULTIPLE_OBJS)
-		perror("Error: multiple cameras or ambient lights detected\n");
+		ft_putstr_fd("Error: multiple cameras or ambient lights detected\n", 2);
 }
 
 void	print_exit_msg(int exit_code)
